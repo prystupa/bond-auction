12f618b6b40f755916ff86eb3d0b8cfe3c10c64f
prystupa <eugene.prystupa@gmail.com>
2018-11-29 21:20:15 -0500

    enable redux dev tools

--- trader-ui/src/redux/store.ts
+++ trader-ui/src/redux/store.ts
@@ -1,4 +1,4 @@
-import {applyMiddleware, createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import {createEpicMiddleware} from "redux-observable";
 
 import {rootEpic, rootReducer} from "./modules";
@@ -6,9 +6,11 @@ import {rootEpic, rootReducer} from "./modules";
 const epicMiddleware = createEpicMiddleware();
 
 function configureStore() {
+    const environment = window as any;
+    const composeEnhancers = environment.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     const store = createStore(
         rootReducer,
-        applyMiddleware(epicMiddleware)
+        composeEnhancers(applyMiddleware(epicMiddleware))
     );
 
     epicMiddleware.run(rootEpic);
