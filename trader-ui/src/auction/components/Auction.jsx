d2715ac6fb4469ab9c999e1ba64dbad467f660a8
prystupa <eugene.prystupa@gmail.com>
2018-12-21 14:24:47 -0500

    add ability to place orders via UI, so Postman is not required

--- trader-ui/src/auction/components/Auction.jsx
+++ trader-ui/src/auction/components/Auction.jsx
@@ -3,39 +3,54 @@ import {CircularProgress, Typography, Button, Grid} from "@material-ui/core";
 import {Done, ErrorOutline as Error} from "@material-ui/icons";
 import {connect} from "react-redux";
 
-import {createAuction} from "../../redux/modules/auction";
+import {createAuction, placeBid} from "../../redux/modules/auction";
 
-function Auction({fetching, auction, error, messages, createAuction}) {
-    return (
-        <Grid container={true} spacing={8}>
-            <Grid container={true} item={true} alignItems="center" spacing={8}>
-                <Grid item={true}>
-                    <Button variant="contained" onClick={createAuction}>Create Auction</Button>
+class Auction extends React.PureComponent {
+    _placeBid = () => {
+        const {placeBid, auction: {id}} = this.props;
+        placeBid(id);
+    };
+
+    render() {
+        const {fetching, auction, error, messages, createAuction} = this.props;
+
+        return (
+            <Grid container={true} spacing={8}>
+                <Grid container={true} item={true} alignItems="center" spacing={8}>
+                    <Grid item={true}>
+                        <Button variant="contained" onClick={createAuction}>Create Auction</Button>
+                    </Grid>
+                    {fetching &&
+                    <Grid item={true}><CircularProgress size={20}/></Grid>}
+                    {auction && <Grid item={true}><Done color="primary"/></Grid>}
+                    {error && <Grid item={true}><Error color="error"/></Grid>}
                 </Grid>
-                {fetching &&
-                <Grid item={true}><CircularProgress size={20}/></Grid>}
-                {auction && <Grid item={true}><Done color="primary"/></Grid>}
-                {error && <Grid item={true}><Error color="error"/></Grid>}
-            </Grid>
 
-            {auction &&
-            <Grid container={true} item={true}>{JSON.stringify(auction)}</Grid>}
+                {auction &&
+                <>
+                    <Grid item={true} xs={12}>{JSON.stringify(auction)}</Grid>
+                    <Grid item={true}>
+                        <Button variant="contained" onClick={this._placeBid}>Place Bid</Button>
+                    </Grid>
+                </>
+                }
 
-            <Grid item={true} xs={12}>
-                <Typography variant={"h6"}>Blotter</Typography>
+                <Grid item={true} xs={12}>
+                    <Typography variant={"h6"}>Blotter</Typography>
+                </Grid>
+                {messages.length === 0 &&
+                <Grid item={true} xs={12}>
+                    <Typography variant="body1">You have no messages to display</Typography>
+                </Grid>
+                }
+                {messages.length > 0 &&
+                <Grid item={true} xs={12}>
+                    <Typography variant="body1">You have {messages.length} messages</Typography>
+                </Grid>
+                }
             </Grid>
-            {messages.length === 0 &&
-            <Grid item={true} xs={12}>
-                <Typography variant="body1">You have no messages to display</Typography>
-            </Grid>
-            }
-            {messages.length > 0 &&
-            <Grid item={true} xs={12}>
-                <Typography variant="body1">You have {messages.length} messages</Typography>
-            </Grid>
-            }
-        </Grid>
-    );
+        );
+    }
 }
 
 const stateMap = (
@@ -44,4 +59,4 @@ const stateMap = (
         blotter: {messages}
     }) => ({fetching, auction, error, messages});
 
-export default connect(stateMap, {createAuction})(Auction);
+export default connect(stateMap, {createAuction, placeBid})(Auction);
--- trader-ui/src/redux/modules/auction/index.js
+++ trader-ui/src/redux/modules/auction/index.js
@@ -1,13 +1,19 @@
 import {flatMap} from "rxjs/operators";
-import {ofType} from "redux-observable";
+import {combineEpics, ofType} from "redux-observable";
 
 const AUCTION_CREATE = "auction://create";
 const AUCTION_CREATED = "auction://created";
 const AUCTION_CREATE_FAILED = "auction://create-failed";
+const PLACE_BID = 'auction://place-bid';
+const BID_PLACED = 'auction://bid-placed';
+const PLACE_BID_FAILED = 'auction://place-bid-failed';
 
 const createAuction = () => ({type: AUCTION_CREATE});
 const auctionCreated = (auction) => ({type: AUCTION_CREATED, auction});
 const createAuctionFailed = (error) => ({type: AUCTION_CREATE_FAILED, error});
+const placeBid = (auctionId) => ({type: PLACE_BID, auctionId});
+const bidPlaced = (order) => ({type: BID_PLACED, order});
+const placeBidFailed = (error) => ({type: PLACE_BID_FAILED, error});
 
 const INITIAL_STATE = {
     fetching: false,
@@ -66,6 +72,29 @@ function createAuctionEpic(action$) {
     );
 }
 
-export {createAuction};
-export {createAuctionEpic as auctionEpic};
+function placeBidEpic(action$) {
+    return action$.pipe(
+        ofType(PLACE_BID),
+        flatMap(async ({auctionId}) => {
+                try {
+                    const response = await fetch(
+                        `/api/auctions/${auctionId}/orders`,
+                        {
+                            method: 'POST'
+                        });
+
+                    const order = await response.json();
+                    return bidPlaced(order);
+                } catch (error) {
+                    return placeBidFailed(error);
+                }
+            }
+        )
+    );
+}
+
+const auctionEpic = combineEpics(createAuctionEpic, placeBidEpic);
+
+export {createAuction, placeBid};
+export {auctionEpic};
 export default auction;
