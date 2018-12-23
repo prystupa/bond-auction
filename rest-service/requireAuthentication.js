a38aab604409f6cf98c8ddf12c926a1da2832136
prystupa <eugene.prystupa@gmail.com>
2018-12-23 14:25:27 -0500

    truly verify access tokens on REST calls

--- rest-service/requireAuthentication.js
+++ rest-service/requireAuthentication.js
@@ -1,3 +1,14 @@
+const OktaJwtVerifier = require("@okta/jwt-verifier");
+
+const config = {
+    issuer: `${process.env.APP_OKTA_ORG_URL}/oauth2/default`,
+    clientId: `${process.env.APP_OKTA_CLIENT_ID}`,
+    assertClaims: {
+        aud: 'api://default'
+    }
+};
+const oktaJwtVerifier = new OktaJwtVerifier(config);
+
 function requireAuthentication(req, res, next) {
 
     const authHeader = req.headers.authorization || '';
@@ -7,7 +18,15 @@ function requireAuthentication(req, res, next) {
         return res.status(401).end();
     }
 
-    next();
+    const accessToken = match[1];
+
+    return oktaJwtVerifier.verifyAccessToken(accessToken)
+        .then(jwt => {
+            req.jwt = jwt;
+            next();
+        }).catch(err => {
+            res.status(401).send(err.message);
+        });
 }
 
 module.exports = requireAuthentication;
