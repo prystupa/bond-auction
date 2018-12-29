6c560707c143fbfc5758d520f1ba10bd36afeaa0
prystupa <eugene.prystupa@gmail.com>
2018-12-29 16:36:51 -0500

    simple push server using Vert.x

--- push-service/src/main/java/com/prystupa/PushService.java
+++ push-service/src/main/java/com/prystupa/PushService.java
@@ -3,14 +3,23 @@ package com.prystupa;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.ext.stomp.StompServer;
+import io.vertx.ext.stomp.StompServerHandler;
+import io.vertx.ext.stomp.StompServerOptions;
 
 public class PushService extends AbstractVerticle {
     @Override
     public void start(Future<Void> startFuture) {
 
+        StompServer stompServer = StompServer.create(vertx, new StompServerOptions()
+                .setPort(-1)
+                .setWebsocketBridge(true)
+                .setWebsocketPath("/ws")
+        ).handler(StompServerHandler.create(vertx));
+
         vertx.createHttpServer()
-                .requestHandler(r -> r.response().end("Hello from Vert.x!"))
-                .listen(8080, result -> {
+                .websocketHandler(stompServer.webSocketHandler())
+                .listen(8089, result -> {
                     if (result.succeeded()) {
                         startFuture.complete();
                     } else {
