b5f9bd03a5763b0e7bf7aacab8808130871d9b8f
eprystupa <eugene.prystupa@gmail.com>
2018-12-29 21:33:32 -0500

    correctly configure push-service for STOMP

--- push-service/src/main/java/com/prystupa/PushService.java
+++ push-service/src/main/java/com/prystupa/PushService.java
@@ -3,6 +3,7 @@ package com.prystupa;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerOptions;
 import io.vertx.ext.stomp.StompServer;
 import io.vertx.ext.stomp.StompServerHandler;
 import io.vertx.ext.stomp.StompServerOptions;
@@ -11,15 +12,18 @@ public class PushService extends AbstractVerticle {
     @Override
     public void start(Future<Void> startFuture) {
 
-        StompServer stompServer = StompServer.create(vertx, new StompServerOptions()
+        StompServerOptions stompServerOptions = new StompServerOptions()
                 .setPort(-1)
                 .setWebsocketBridge(true)
-                .setWebsocketPath("/ws")
-        ).handler(StompServerHandler.create(vertx));
+                .setWebsocketPath("/ws");
+        StompServer stompServer = StompServer.create(vertx, stompServerOptions)
+                .handler(StompServerHandler.create(vertx));
 
-        vertx.createHttpServer()
+        HttpServerOptions httpServerOptions = new HttpServerOptions()
+                .setWebsocketSubProtocols("v10.stomp, v11.stomp, v12.stomp");
+        vertx.createHttpServer(httpServerOptions)
                 .websocketHandler(stompServer.webSocketHandler())
-                .listen(8089, result -> {
+                .listen(8083, result -> {
                     if (result.succeeded()) {
                         startFuture.complete();
                     } else {
