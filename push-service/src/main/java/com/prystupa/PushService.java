8cb7b3d67f601713693d26fa4f8eba79f55f237d
eprystupa <eugene.prystupa@gmail.com>
2019-01-12 14:43:00 -0500

    implement different strategy of broadcasting blotter updates using Rx, so that entitlements can be plugged in per each web-socket subscriber

--- push-service/src/main/java/com/prystupa/PushService.java
+++ push-service/src/main/java/com/prystupa/PushService.java
@@ -1,6 +1,8 @@
 package com.prystupa;
 
 
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerOptions;
@@ -10,14 +12,14 @@ import io.vertx.ext.stomp.StompServerOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-
 public class PushService extends AbstractVerticle {
     private static Logger logger = LoggerFactory.getLogger(PushService.class);
 
     @Override
-    public void start(Future<Void> startFuture) throws IOException {
+    public void start(Future<Void> startFuture) throws Exception {
         logger.info("Starting push service");
+
+        Connection rabbitConnection = connectToRabbit();
 
         StompServerOptions stompServerOptions = new StompServerOptions()
                 .setPort(-1)
@@ -26,7 +28,7 @@ public class PushService extends AbstractVerticle {
                 .setWebsocketPath("/ws");
         StompServer stompServer = StompServer.create(vertx, stompServerOptions)
                 .handler(StompServerHandler.create(vertx)
-                        .destinationFactory(RabbitDestination::factory)
+                        .destinationFactory((vertx, name) -> RabbitDestination.factory(vertx, name, rabbitConnection))
                         .authProvider(new OktaAuthProvider())
                         .connectHandler(new OktaConnectHandler()));
 
@@ -34,13 +36,12 @@ public class PushService extends AbstractVerticle {
                 .setWebsocketSubProtocols("v10.stomp, v11.stomp, v12.stomp");
         vertx.createHttpServer(httpServerOptions)
                 .websocketHandler(stompServer.webSocketHandler())
-                .listen(8083, result -> {
-                    if (result.succeeded()) {
-                        logger.info("Started websocket STOMP server");
-                        startFuture.complete();
-                    } else {
-                        startFuture.fail(result.cause());
-                    }
-                });
+                .listen(8083, startFuture.mapEmpty());
+    }
+
+    private Connection connectToRabbit() throws Exception {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setUri("amqp://message-bus");
+        return connectionFactory.newConnection();
     }
 }
--- push-service/src/main/java/com/prystupa/RabbitDestination.java
+++ push-service/src/main/java/com/prystupa/RabbitDestination.java
@@ -1,19 +1,19 @@
 package com.prystupa;
 
-import io.vertx.core.Future;
+import com.rabbitmq.client.*;
+import io.reactivex.Observable;
+import io.reactivex.subjects.BehaviorSubject;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.auth.User;
 import io.vertx.ext.stomp.Destination;
 import io.vertx.ext.stomp.Frame;
 import io.vertx.ext.stomp.StompServerConnection;
 import io.vertx.ext.stomp.impl.Topic;
-import io.vertx.rabbitmq.RabbitMQClient;
-import io.vertx.rabbitmq.RabbitMQConsumer;
-import io.vertx.rabbitmq.RabbitMQOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -21,43 +21,51 @@ import java.util.regex.Pattern;
 public class RabbitDestination extends Topic {
     private static Logger logger = LoggerFactory.getLogger(RabbitDestination.class);
     private static Pattern destinationPattern = Pattern.compile("/exchange/(?<exchange>.+)/(?<pattern>.+)");
-    private final RabbitMQClient client;
-    private final String exchange;
-    private final String pattern;
+    private final Observable<byte[]> messages;
 
-    static Destination factory(Vertx vertx, String name) {
+    static Destination factory(Vertx vertx, String name, Connection rabbitConnection) {
+        logger.debug("Creating destination for {}", name);
+
         Matcher matcher = destinationPattern.matcher(name);
         if (!matcher.matches()) {
             logger.error("Invalid destination", name);
             return null;
         }
 
-        String exchange = matcher.group("exchange");
-        String pattern = matcher.group("pattern");
-        return new RabbitDestination(vertx, name, exchange, pattern);
+        try {
+            String exchange = matcher.group("exchange");
+            String pattern = matcher.group("pattern");
+            BehaviorSubject<byte[]> messages = BehaviorSubject.create();
+
+            Channel channel = rabbitConnection.createChannel();
+            String queue = "push-service-" + UUID.randomUUID();
+            channel.queueDeclare(queue, false, true, true, Collections.emptyMap());
+            channel.queueBind(queue, exchange, pattern);
+            channel.basicConsume(queue, true, new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
+                    logger.debug("Received Rabbit message: {}", new String(body));
+                    messages.onNext(body);
+                }
+
+                @Override
+                public void handleShutdownSignal(String consumerTag, ShutdownSignalException sig) {
+                    logger.error("Received Rabbit shutdown signal");
+                    messages.onError(sig);
+                }
+            });
+
+            return new RabbitDestination(vertx, name, messages);
+        } catch (Exception e) {
+            logger.error("Something went wrong when setting up destination", e);
+            return null;
+        }
     }
 
-    private RabbitDestination(Vertx vertx, String name, String exchange, String pattern) {
-        super(vertx, name);
-        this.exchange = exchange;
-        this.pattern = pattern;
+    private RabbitDestination(Vertx vertx, String destination, Observable<byte[]> messages) {
+        super(vertx, destination);
 
-        logger.debug("Creating RabbitMQ destination");
-
-        RabbitMQOptions options = new RabbitMQOptions().setUri("amqp://message-bus");
-        client = RabbitMQClient.create(vertx, options);
-
-        startRabbitMqClient()
-                .compose(v -> declareQueue())
-                .compose(queueName -> bindQueue(queueName)
-                        .compose(v -> setupConsumer(queueName)))
-                .setHandler(result -> {
-                    if (result.failed()) {
-                        logger.error("Something went wrong with RabbitMQ: {}", result.cause());
-                    } else {
-                        logger.debug("All went well with RabbitMQ.");
-                    }
-                });
+        this.messages = messages;
     }
 
     @Override
@@ -67,54 +75,34 @@ public class RabbitDestination extends Topic {
 
     @Override
     public synchronized Destination subscribe(StompServerConnection connection, Frame frame) {
+        logger.debug("Handling new subscription, have {} subscriptions before adding this one", subscriptions.size());
+
         User user = connection.handler().getUserBySession(connection.session());
         String userId = user.principal().getString("user");
 
         logger.debug("Handling Subscribe frame to {} for {}...", frame.getDestination(), userId);
 
+        messages.subscribe(message -> {
+            logger.debug("Message arrived, dispatching to {}: {}", userId, new String(message));
+
+            Frame stompFrame = transform(
+                    new Frame().setBody(Buffer.buffer(message)),
+                    new StompSubscription(connection, frame),
+                    UUID.randomUUID().toString());
+            connection.write(stompFrame);
+        }, error -> {
+            connection.write(new Frame(Frame.Command.ERROR, Collections.emptyMap(), Buffer.buffer()));
+            connection.close();
+        });
+
         super.subscribe(connection, frame);
+        logger.debug("Added new subscription, now have {}", subscriptions.size());
         return this;
     }
 
-    private Future<Void> startRabbitMqClient() {
-        Future<Void> clientStarted = Future.future();
-        client.start(clientStarted);
-        return clientStarted.compose(v -> {
-            logger.debug("Successfully started RabbitMQ client.");
-            return clientStarted;
-        });
-    }
-
-    private Future<String> declareQueue() {
-        Future<JsonObject> queueDeclared = Future.future();
-        String queueName = "push-service-" + UUID.randomUUID();
-        client.queueDeclare(queueName, false, true, true, queueDeclared);
-        return queueDeclared.compose(json -> {
-            logger.debug("Succeeded in declaring a queue: {} for {}", queueName);
-            return queueDeclared.map(queueName);
-        });
-    }
-
-    private Future<Void> bindQueue(String queueName) {
-        Future<Void> queueBound = Future.future();
-        client.queueBind(queueName, exchange, pattern, queueBound);
-        return queueBound.compose(v -> {
-            logger.debug("Succeeded in binding queue {} to {}/{}", queueName, exchange, pattern);
-            return queueBound;
-        });
-    }
-
-    private Future<RabbitMQConsumer> setupConsumer(String queueName) {
-        Future<RabbitMQConsumer> consumerSetup = Future.future();
-        client.basicConsumer(queueName, consumerSetup);
-        return consumerSetup.compose(consumer -> {
-            consumer.handler(message -> {
-                logger.debug("Message arrived, dispatching: {}", message.body().toString());
-                dispatch(null, new Frame().setBody(message.body()));
-            });
-
-            logger.debug("Setup consumer {} for {}", consumer.consumerTag(), queueName);
-            return consumerSetup;
-        });
+    private static class StompSubscription extends Subscription {
+        StompSubscription(StompServerConnection connection, Frame frame) {
+            super(connection, frame);
+        }
     }
 }
